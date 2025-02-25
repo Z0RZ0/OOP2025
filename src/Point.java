562fec03e0ec87d2a02db079453c152a7bfe0f56
Z0RZ0 <kacper.ciozda@wp.pl>
2025-02-25 19:31:23 +0100

    lab1

--- src/Point.java
+++ src/Point.java
@@ -6,4 +6,9 @@ public class Point {
         return "x= "+x+" y= "+y;
     }
 
+    public String toSvg(){
+        return "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <circle r=\"45\" cx=\"x\" cy=\"y\" fill=\"red\" />\n" +
+                "</svg>";
+    }
 }
