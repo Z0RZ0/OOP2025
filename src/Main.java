a159d47c0a8f2d9b2d215b541b1e39d6c8722578
Z0RZ0 <kacper.ciozda@wp.pl>
2025-02-25 19:41:14 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
        Point p1 = new Point();
-       p1.x=600;
+       p1.x=100;
        p1.y=100;
 
        System.out.println(p1.toSvg());
--- src/Point.java
+++ src/Point.java
@@ -7,7 +7,7 @@ public class Point {
     }
 
     public String toSvg(){
-        return "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        return "<svg height=\"500\" width=\"500\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
                 "  <circle r=\"45\" cx=\""+x+"\" cy=\""+y+"\" fill=\"red\" />\n" +
                 "</svg>";
     }
